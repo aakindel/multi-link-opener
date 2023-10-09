b3df8c09ea56799a7ac5cd22bc8c62af78903207
aakindel <a.akindele01@gmail.com>
2023-10-09 00:03:07 -0600

    create layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ const Home: NextPage = () => {
   return (
     <div className="mx-auto min-h-screen w-full">
       <MainNav />
-      <div className="mx-auto min-h-[calc(100vh-57px)] w-full max-w-[1400px] px-4 pt-4 sm:px-8">
-        <p className="font-medium">Default template removed.</p>
+      <div className="mx-auto flex h-[calc(100vh-57px)] w-full max-w-6xl items-center px-4 pt-0 sm:px-8">
+        <div className="flex h-[80%] w-full gap-4">
+          <div className="flex h-full w-[350px] shrink-0 border"></div>
+          <div className="flex h-full w-full flex-1 border"></div>
+        </div>
       </div>
     </div>
   );
