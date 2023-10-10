99496d2c65f25c331f3360f6cc2eb32ea8924400
aakindel <a.akindele01@gmail.com>
2023-10-10 00:20:26 -0600

    create buttons for adding link sets

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { MainNav } from "@/components/main-nav";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -7,8 +9,14 @@ const Home: NextPage = () => {
       <MainNav />
       <div className="mx-auto flex h-[calc(100vh-57px)] w-full max-w-6xl items-center px-4 pt-0 sm:px-8">
         <div className="flex h-[80%] w-full gap-4">
-          <div className="flex h-full w-[350px] shrink-0 border"></div>
-          <div className="flex h-full w-full flex-1 border"></div>
+          <div className="flex h-full w-[300px] shrink-0 flex-col gap-2">
+            <Button className="gap-2">
+              <PlusCircleIcon className="h-5 w-5" />
+              Add a Link Set
+            </Button>
+            <Button variant="outline">(or prefill 5 demo link sets)</Button>
+          </div>
+          <div className="flex h-full w-full flex-1 rounded-md border"></div>
         </div>
       </div>
     </div>
