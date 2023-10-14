02eced4a6fd3ac6d94085cd60a73d3cff6b19c5d
aakindel <a.akindele01@gmail.com>
2023-10-14 14:58:27 -0600

    add prefill link set data

---
+++ app/data.ts
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const prefillLinks = [
+  {
+    id: uuidv4(),
+    name: "UI Components",
+    links: [
+      "https://tailwindui.com/components",
+      "https://pro.chakra-ui.com/components",
+      "https://www.untitledui.com/components",
+      "https://ui.shadcn.com/components",
+      "https://kit.uifoundations.com/",
+    ],
+  },
+  {
+    id: uuidv4(),
+    name: "Web Demos",
+    links: [
+      "https://demo.withcompound.com/wealth/accounts",
+      "https://convertkit.baremetrics.com/",
+      "https://demo.mercury.com/dashboard",
+      "https://plausible.io/plausible.io",
+      "https://www.remnote.com/notes",
+    ],
+  },
+  {
+    id: uuidv4(),
+    name: "Productivity Stack",
+    links: [
+      "https://www.youtube.com/watch?v=wf4AoGYsZys",
+      "https://www.youtube.com/watch?v=-5_NiRTS2nE",
+      "https://vclock.com/set-timer-for-1-hour/",
+    ],
+  },
+  {
+    id: uuidv4(),
+    name: "Site Curators",
+    links: [
+      "https://godly.website/",
+      "https://minimal.gallery/",
+      "https://www.awwwards.com/websites/",
+    ],
+  },
+  {
+    id: uuidv4(),
+    name: "Interactive Articles",
+    links: [
+      "https://blog.maximeheckel.com/posts/guide-animations-spark-joy-framer-motion/",
+      "https://www.joshwcomeau.com/css/surprising-truth-about-pixels-and-accessibility/",
+      "http://worrydream.com/LadderOfAbstraction/",
+      "https://www.nan.fyi/svg-paths",
+    ],
+  },
+];
