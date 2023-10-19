1d81487c10c8ce58ed28b5d9d150083611d61369
aakindel <a.akindele01@gmail.com>
2023-10-19 15:04:13 -0600

    add functionality to edit link sets

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AddLinkSetForm } from "@/components/link-set-form";
+import { AddLinkSetForm, EditLinkSetForm } from "@/components/link-set-form";
 import { MainNav } from "@/components/main-nav";
 import { Button } from "@/components/ui/button";
 import {
@@ -31,6 +31,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { LinkSetType } from "@/types";
 
 const TooltipButton = ({
   icon,
@@ -62,14 +63,123 @@ const TooltipButton = ({
   );
 };
 
+const AddLinkSetDialog = ({
+  showAddLinkSetDialog,
+  setShowAddLinkSetDialog,
+  addLinkSetFormRef,
+}: {
+  showAddLinkSetDialog: boolean;
+  setShowAddLinkSetDialog: React.Dispatch<React.SetStateAction<boolean>>;
+  addLinkSetFormRef: React.RefObject<HTMLFormElement>;
+}) => {
+  return (
+    <Dialog open={showAddLinkSetDialog} onOpenChange={setShowAddLinkSetDialog}>
+      <DialogTrigger asChild>
+        <Button className="shrink-0 gap-2">
+          <PlusCircleIcon className="h-5 w-5" />
+          Add a Link Set
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="gap-3 p-4">
+        <DialogHeader className="mt-1 space-y-3">
+          <DialogTitle>Add Link Set</DialogTitle>
+          <DialogDescription>
+            Name your link set and insert your web link(s) in the text area
+            below. Click the Add Link Set button when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col gap-2">
+          <AddLinkSetForm
+            addLinkSetFormRef={addLinkSetFormRef}
+            closeDialog={() => setShowAddLinkSetDialog(false)}
+          />
+        </div>
+        <DialogFooter className="mt-1">
+          <Button
+            variant="outline"
+            onClick={() => setShowAddLinkSetDialog(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            onClick={() => addLinkSetFormRef.current?.requestSubmit()}
+          >
+            Add Link Set
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const EditLinkSetDialog = ({
+  linkSet,
+  showEditLinkSetDialog,
+  setShowEditLinkSetDialog,
+  editLinkSetFormRef,
+}: {
+  linkSet: LinkSetType | null;
+  showEditLinkSetDialog: boolean;
+  setShowEditLinkSetDialog: React.Dispatch<React.SetStateAction<boolean>>;
+  editLinkSetFormRef: React.RefObject<HTMLFormElement>;
+}) => {
+  return (
+    linkSet && (
+      <Dialog
+        open={showEditLinkSetDialog}
+        onOpenChange={setShowEditLinkSetDialog}
+      >
+        <DialogContent className="gap-3 p-4">
+          <DialogHeader className="mt-1 space-y-3">
+            <DialogTitle>Edit Link Set</DialogTitle>
+            <DialogDescription>
+              Edit your link set name and link(s) below. Click the Save Changes
+              button when you&apos;re done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-2">
+            <EditLinkSetForm
+              linkSet={linkSet}
+              editLinkSetFormRef={editLinkSetFormRef}
+              closeDialog={() => setShowEditLinkSetDialog(false)}
+            />
+          </div>
+          <DialogFooter className="mt-1">
+            <Button
+              variant="outline"
+              onClick={() => setShowEditLinkSetDialog(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              onClick={() => editLinkSetFormRef.current?.requestSubmit()}
+            >
+              Save Changes
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    )
+  );
+};
+
 const Home: NextPage = () => {
   const addedLinkSets = useLinkStore((state) => state.addedLinkSets);
   const setAddedLinkSets = useLinkStore((state) => state.setAddedLinkSets);
   const [activeTab, setActiveTab] = useState(
     addedLinkSets.length ? addedLinkSets[0].id : ""
   );
+
   const addLinkSetFormRef = useRef<HTMLFormElement>(null);
   const [showAddLinkSetDialog, setShowAddLinkSetDialog] = useState(false);
+
+  const editLinkSetFormRef = useRef<HTMLFormElement>(null);
+  const [editingLinkset, setEditingLinkset] = useState<LinkSetType | null>(
+    null
+  );
+  const [showEditLinkSetDialog, setShowEditLinkSetDialog] = useState(false);
 
   return (
     <div className="mx-auto min-h-screen w-full">
@@ -85,47 +195,17 @@ const Home: NextPage = () => {
         >
           <div className="flex h-[80%] w-full gap-4">
             <div className="flex h-full w-[300px] shrink-0 flex-col gap-2">
-              <Dialog
-                open={showAddLinkSetDialog}
-                onOpenChange={setShowAddLinkSetDialog}
-              >
-                <DialogTrigger asChild>
-                  <Button className="shrink-0 gap-2">
-                    <PlusCircleIcon className="h-5 w-5" />
-                    Add a Link Set
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="gap-3 p-4">
-                  <DialogHeader className="mt-1 space-y-3">
-                    <DialogTitle>Add Link Set</DialogTitle>
-                    <DialogDescription>
-                      Name your link set and insert your web link(s) in the text
-                      area below. Click the Add Link Set button when you&apos;re
-                      done.
-                    </DialogDescription>
-                  </DialogHeader>
-                  <div className="flex flex-col gap-2">
-                    <AddLinkSetForm
-                      addLinkSetFormRef={addLinkSetFormRef}
-                      closeDialog={() => setShowAddLinkSetDialog(false)}
-                    />
-                  </div>
-                  <DialogFooter className="mt-1">
-                    <Button
-                      variant="outline"
-                      onClick={() => setShowAddLinkSetDialog(false)}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      type="submit"
-                      onClick={() => addLinkSetFormRef.current?.requestSubmit()}
-                    >
-                      Add Link Set
-                    </Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
+              <AddLinkSetDialog
+                showAddLinkSetDialog={showAddLinkSetDialog}
+                setShowAddLinkSetDialog={setShowAddLinkSetDialog}
+                addLinkSetFormRef={addLinkSetFormRef}
+              />
+              <EditLinkSetDialog
+                linkSet={editingLinkset}
+                showEditLinkSetDialog={showEditLinkSetDialog}
+                setShowEditLinkSetDialog={setShowEditLinkSetDialog}
+                editLinkSetFormRef={editLinkSetFormRef}
+              />
               {!addedLinkSets.length && (
                 <Button
                   variant="outline"
@@ -176,6 +256,10 @@ const Home: NextPage = () => {
                                   <PenSquareIcon className="h-4 w-4 stroke-2" />
                                 }
                                 tooltipText="Edit Link Set"
+                                onClick={() => {
+                                  setEditingLinkset(addedLinkSet);
+                                  setShowEditLinkSetDialog(true);
+                                }}
                               />
                               <TooltipButton
                                 icon={
