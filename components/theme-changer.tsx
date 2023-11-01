4b8c69ce27e61c2b9ebf938a79fe1ac9d2d1f51c
aakindel <a.akindele01@gmail.com>
2023-11-01 14:13:09 -0600

    create theme changer switch component

--- components/theme-changer.tsx
+++ components/theme-changer.tsx
@@ -5,6 +5,7 @@ import { useTheme } from "next-themes";
 import useHasWindow from "@/hooks/useHasWindow";
 import { Button } from "./ui/button";
 import { Icons } from "./icons";
+import { Switch } from "./ui/switch";
 
 const ThemeChanger = () => {
   const { resolvedTheme, setTheme } = useTheme();
@@ -35,6 +36,28 @@ const ThemeChanger = () => {
           <Icons.moon className="absolute mt-px h-[1.2rem] w-[1.2rem]" />
         </Button>
       )}
+    </React.Fragment>
+  ) : (
+    <div className="h-9 w-9 px-0"></div>
+  );
+};
+
+export const ThemeChangerSwitch = ({
+  isDarkMode,
+  changeTheme,
+}: {
+  isDarkMode: boolean;
+  changeTheme: () => void;
+}) => {
+  const hasWindow = useHasWindow();
+  return hasWindow ? (
+    <React.Fragment>
+      <Switch
+        name="dark_mode"
+        id="dark_mode"
+        checked={isDarkMode}
+        onCheckedChange={changeTheme}
+      />
     </React.Fragment>
   ) : (
     <div className="h-9 w-9 px-0"></div>
